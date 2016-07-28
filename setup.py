a6e14d3da3a45e7a2df3c599e50ba6c563da609f
AurelienNioche <nioche.aurelien@gmail.com>
2016-07-28 16:21:41 +0200

    update

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ from Cython.Distutils import build_ext
 import numpy as np
 
 extensions = [ 
-    Extension("cython_economy", ["cython_economy.pyx"], include_dirs=[np.get_include()])
+    Extension("eco.c_economy", ["eco/c_economy.pyx"], include_dirs=[np.get_include()])
 
 ]
 
 setup(
-    name="cython_economy",
+    name="c_economy",
     ext_modules = cythonize(extensions),
 )
