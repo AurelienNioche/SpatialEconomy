cb63f8f6e64af600c83109f12d3005ff1807389f
AurelienNioche <nioche.aurelien@gmail.com>
2016-09-29 10:46:14 +0200

    update paramters for avakas

--- arborescence/arborescence.py
+++ arborescence/arborescence.py
@@ -6,7 +6,7 @@ class Folders:
     folders = OrderedDict(
         [
             ("scripts", "../avakas_scripts"),
-            ("jobs", "../job_names"),
+            ("job_names", "../job_names"),
             ("parameters", "../avakas_input_parameters"),
             ("logs", "../avakas_logs"),
             ("data", "../data")
